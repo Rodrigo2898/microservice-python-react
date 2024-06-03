de18eb3d9fb6cdcdca8c3e510b8d123e2c9e2986
Rodrigo2898 <rodrigofeitosarodrigues@gmail.com>
2024-06-02 21:26:20 -0300

    Flask migrations

--- backend/flaskMain/app.py
+++ backend/flaskMain/app.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
 CORS(app)
 
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Product(db.Model):
